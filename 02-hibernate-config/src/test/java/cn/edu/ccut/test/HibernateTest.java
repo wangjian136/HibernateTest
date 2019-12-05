bb0745aa8d2faa1440e457c68835fce4a8af17dc
wangjian136 <1451518614@qq.com>
2019-12-05 21:36:06 +0800

    something change

--- 02-hibernate-config/src/test/java/cn/edu/ccut/test/HibernateTest.java
+++ 02-hibernate-config/src/test/java/cn/edu/ccut/test/HibernateTest.java
@@ -1,11 +1,11 @@
 package cn.edu.ccut.test;
 
-import java.io.InputStream;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,10 +22,45 @@ public class HibernateTest {
 	}
 
 	@Test
-	public void testHibernateInit() {
+	public void testHibernateAdd() {
 		try (Session session = factory.openSession();) {
-			Blog blog = session.get(Blog.class, 101);
-	        System.out.println(blog);
+			session.beginTransaction();
+			Blog blog = new Blog(null, "C++", "C++开发");
+			session.save(blog);
+			session.getTransaction().commit();
+		}
+	}
+	
+	@Test
+	public void testHibernateUpdate() {
+		try (Session session = factory.openSession();) {
+			session.beginTransaction();
+			/*Blog blog = new Blog(108, "C++", "test,C++开发");
+			session.saveOrUpdate(blog);*/
+			Blog blog = session.get(Blog.class, 108);
+			blog.setDescs("sss");
+			session.update(blog);
+			session.getTransaction().commit();
+		}
+	}
+	
+	
+	@Test
+	public void testHibernateDelete() {
+		try (Session session = factory.openSession();) {
+			session.beginTransaction();
+			Blog blog = session.get(Blog.class, 108);
+			session.delete(blog);
+			session.getTransaction().commit();
+		}
+	}
+	
+	@Test
+	public void testHibernateSelect() {
+		try (Session session = factory.openSession();) {
+			Query<Blog> query = session.createQuery("from Blog");
+			List<Blog> blogs = query.list();
+			System.out.println(blogs);
 		}
 	}
 }
